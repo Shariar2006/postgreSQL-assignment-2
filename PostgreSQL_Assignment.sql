914814f7b14591eaffc07feed0f35fb8ccbf1e56
Shariar2006 <shariarnafis86@gmail.com>
2025-05-25 20:48:41 +0600

    solve 1st and 2nd problem

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -75,38 +75,46 @@ INSERT INTO
         sighting_time,
         notes
     )
-VALUES 
-(
-    1,
-1,
+VALUES (
+        1,
+        1,
         'Peak Ridge',
         '2024-05-10 07:45:00',
         'Camera trap image captured'
     ),
-(
-    2,
-2,
+    (
+        2,
+        2,
         'Bankwood Area',
         '2024-05-12 16:20:00',
         'Juvenile seen'
     ),
-(
-    3,
-3,
+    (
+        3,
+        3,
         'Bamboo Grove East',
         '2024-05-15 09:10:00',
         'Feeding observed'
     ),
-(
-    1,
-2,
+    (
+        1,
+        2,
         'Snowfall Pass',
         '2024-05-18 18:30:00',
         NULL
     );
 
-
 SELECT * FROM rangers;
+
 SELECT * FROM species;
+
 SELECT * FROM sightings;
+
+-- problem 1
+INSERT INTO
+    rangers (name, region)
+VALUES ('Derek Fox', 'Coastal Plains');
+
+-- problem 2
+SELECT count(DISTINCT common_name) as unique_species_count FROM species;
 
